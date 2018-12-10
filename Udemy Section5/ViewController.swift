09b5366dc21e999b79fd19a2294c251b427e1aa3
Luthfi Bintoro <luthfi.bintoro@websparks.sg>
2018-12-10 13:10:52 +0700

    Initial Commit

---
+++ Udemy Section5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udemy Section5
+//
+//  Created by WS-Luthfi on 12/10/18.
+//  Copyright © 2018 interfeis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
