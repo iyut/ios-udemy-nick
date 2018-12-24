249c5457714b9d5b147fcdc359967ce6a2db93b1
Luthfi Bintoro <luthfi.bintoro@websparks.sg>
2018-12-24 15:11:50 +0700

    first commit

--- Udemy Section5/ViewController.swift
+++ Udemy Section5/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var test3Label: UILabel!
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var tapCount = 0;
+    var tapString : String = "";
+    
+    @IBAction func buttonCount(_ sender: Any) {
+        
+        if(tapCount<11){
+            tapCount += 1;
+        }
+        
+        tapString = "tap count: " + String(tapCount);
+        
+        countLabel.text = tapString;
+        
+    }
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        self.view.backgroundColor = UIColor.red;
+        
+        test3Label.text = "Hello there";
+        
+        print("button pushed");
+        
+    }
+
+    @IBAction func coolTapped(_ sender: Any) {
+        
+        test3Label.text = "Cool Button has been Pushed!";
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
